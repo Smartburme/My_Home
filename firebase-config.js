84918b0c28eface797c541b0b9f7825d3ffbe685
Smartburme <athuyrain@gmail.com>
2025-04-17 14:29:19 -0400

    Create firebase-config.js
    Signed-off-by: Smartburme <athuyrain@gmail.com>

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAr7Hv2ApKtNTxF11MhT5cuWeg_Dgsh0TY",
+  authDomain: "smart-burme-app.firebaseapp.com",
+  projectId: "smart-burme-app",
+  storageBucket: "smart-burme-app.appspot.com",
+  messagingSenderId: "851502425686",
+  appId: "1:851502425686:web:f29e0e1dfa84794b4abdf7"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
