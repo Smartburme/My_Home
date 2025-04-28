1463bda06aab980d98f7a86a3835fe131ecdf614
Smartburme <athuyrain@gmail.com>
2025-04-28 18:30:06 +0630

    Update bottom.js
    Signed-off-by: Smartburme <athuyrain@gmail.com>

--- bottom.js
+++ bottom.js
@@ -1,12 +1,12 @@
-// Firebase Configuration (Set up your Firebase config here)
+// Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAr7Hv2ApKtNTxF11MhT5cuWeg_Dgsh0TY",
-    authDomain: "smart-burme-app.firebaseapp.com",
-    projectId: "smart-burme-app",
-    storageBucket: "smart-burme-app.appspot.com",
-    messagingSenderId: "851502425686",
-    appId: "1:851502425686:web:f29e0e1dfa84794b4abdf7"
-}
+  apiKey: "AIzaSyAr7Hv2ApKtNTxF11MhT5cuWeg_Dgsh0TY",
+  authDomain: "smart-burme-app.firebaseapp.com",
+  projectId: "smart-burme-app",
+  storageBucket: "smart-burme-app.appspot.com",
+  messagingSenderId: "851502425686",
+  appId: "1:851502425686:web:f29e0e1dfa84794b4abdf7"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
